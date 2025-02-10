f1e13329119264d72c55bc2e088b7a4f9c1a955e
Kalkidan-Gashaw <162141690+Kalkidan-Gashaw@users.noreply.github.com>
2025-02-10 16:20:08 +0300

    Update VerifyEmail.jsx

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -1,22 +1,18 @@
 import React, { useState } from "react"; // Import React and useState
-import { useNavigate } from "react-router-dom"; // Import useNavigate from react-router-dom
+import { useNavigate, useParams } from "react-router-dom"; // Import useNavigate and useParams
 import axiosInstance from "../services"; // Import your axios instance
-import "../index.css";
- // Import your CSS for Tailwind or other styles
+import "../index.css"; // Import your CSS for Tailwind or other styles
 
 const VerifyEmail = () => {
+  const { token } = useParams(); // Get token from URL parameters
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
 
   const handleVerify = async () => {
     setLoading(true);
-    const queryParams = new URLSearchParams(window.location.search);
-    const token = queryParams.get("token"); // Use token
 
     if (!token) {
-      alert(
-        "Invalid token. Please check your email for the verification link."
-      );
+      alert("Invalid token. Please check your email for the verification link.");
       setLoading(false);
       return;
     }
@@ -28,8 +24,7 @@ const VerifyEmail = () => {
     } catch (error) {
       console.log(error);
       const message =
-        error.response?.data?.message ||
-        "Verification failed. Please try again.";
+        error.response?.data?.message || "Verification failed. Please try again.";
       alert(message);
     } finally {
       setLoading(false);
